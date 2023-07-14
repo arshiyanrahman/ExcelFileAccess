5f6114f72344f8d0909b9f799cab8d3bf1a07db6
arshiyanrahman <arshiyan.rahman77@gmail.com>
2023-07-14 18:19:07 +0530

    committed

--- src/app/excelservice.service.ts
+++ src/app/excelservice.service.ts
@@ -10,16 +10,20 @@ export class ExcelserviceService {
 
   constructor(private http : HttpClient) { }
 
+
   refresh = new Subject<void>();
   get Refresh(){
     return this.refresh;
   }
   bookDetails : BookDetails[] = [];
   url1 = "http://localhost:8088/"
+
+  // to return all excel entries stored in db
   getAllExcelData() : Observable<BookDetails[]>{
     return this.http.get<BookDetails[]>(this.url1+"allBooks");
   }
 
+  // uploading excel file and implementing live refresh of data
   uploadExcelFile(formData : FormData){
       this.http.post<any>(this.url1+"books/upload",formData).pipe(tap(()=>this.Refresh.next())).subscribe();
     }
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -9,6 +9,7 @@ import { BookDetails } from '../book-details';
 })
 export class FileUploadComponent {
   constructor(private excelService : ExcelserviceService ){}
+
   file : any;
   excelDetails : BookDetails[] | undefined =[] ;
   ngOnInit(){
@@ -17,23 +18,31 @@ export class FileUploadComponent {
   // this.excelDetails  = resp;
       
   //   });
+
+  // displaying all the books stored in db   
     this.getAll();
+    //to perform live refresh
     this.excelService.Refresh.subscribe((response) =>
     this.getAll())
   }
 
+  // storing excel file uploaded in this.file 
   selectFile(event : Event){
        this.file = (event.target as HTMLInputElement)?.files?.[0];
     }
     
+    // method to upload selected file 
   uploadFile(){
   let formData = new FormData();
   console.log(this.file);
   
+  // appending file uploaded at the end of formData
   formData.append("file",this.file);
+  // uploading formdata to database
   this.excelService.uploadExcelFile(formData);
    }
 
+   // method to get all books stored in db
    getAll(){
     this.excelService.getAllExcelData().toPromise().then(result => this.excelDetails = result)
    }
